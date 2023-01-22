7457fc6449d16e7f78643e88a50189ac72bc55d4
Parag Pallav Singh <40052830+paragpallavsingh@users.noreply.github.com>
2023-01-23 00:17:59 +0530

    add line 2 for new build

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class test{
 	
 	public static void main(String[] arg) {
-		System.out.print("hello world");
+		System.out.println("hello world");
+		System.out.println("Add line two for Jenkins new build");
 	}
 }
