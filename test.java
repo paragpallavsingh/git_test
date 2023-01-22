0942096ecb11a90c509cb38b45ab8d42bf51fa3e
Parag Pallav Singh <40052830+paragpallavsingh@users.noreply.github.com>
2023-01-23 00:43:36 +0530

    Update test.java
    add 1 change for webhook

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ public class test{
 	public static void main(String[] arg) {
 		System.out.println("hello world");
 		System.out.println("Add line two for Jenkins new build");
+		System.out.println("Add line through webhook");
 	}
 }
