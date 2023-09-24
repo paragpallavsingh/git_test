3d5dab9bba0f871f49d2b17d0fad1cb06d9c5b10
Parag Pallav Singh <thakurparag6.1@gmail.com>
2023-09-24 13:18:44 +0530

    revert "add folder" to show the revert command
    This reverts commit f8e3c6c1912a82fcdb4acd089fd971bb9d462511.

--- 2023/move.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {10..90}
-do
-mv day"$i"/tasks.md day"$i"/README.md
-done
--- move.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {10 .. 90}
-do
-echo i
-mv day"$i"/tasks.md day"$i"/README.md
-done
